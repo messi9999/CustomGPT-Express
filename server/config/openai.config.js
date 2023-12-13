2e08a371e80d6274cd506105d614f98aa16f1de8
messi9999 <micklepanda812@gmail.com>
2023-12-13 07:42:11 -0800

    udpated

--- server/config/openai.config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  OPENAI_ASSISTANT_ID: process.env.OPENAI_ASSISTANT_ID,
-  OPENAI_THREAD_ID: process.env.OPENAI_THREAD_ID
-};
