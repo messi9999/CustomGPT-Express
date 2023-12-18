721fc66a12a10d1698066efb69638e9763c67620
messi9999 <micklepanda812@gmail.com>
2023-12-17 16:38:02 -0800

    udpated

--- client/src/pages/chat/ChatPage.js
+++ client/src/pages/chat/ChatPage.js
@@ -43,7 +43,8 @@ function LoadingButton() {
 const DISVOCERS = [
   {
     title: "Create a PCOS Friendly Meal Plan",
-    imgUrl: "/public/assets/images/discover/1.png",
+    imgUrl:
+      "https://pi.ai/_next/image?url=https%3A%2F%2Fpi.ai%2Fpublic%2Fmedia%2Fguided-experiences%2Fventing.jpg&w=256&q=100",
     assistantID: "asst_yoLVpKKIOlDUgPmJDOHN88eh",
     baseContext: `Great! Let’s make a meal plan together. Your meal plan will be PCOS friendly and customized to your needs.
 
@@ -54,7 +55,8 @@ const DISVOCERS = [
   },
   {
     title: "Explore Nutritional Supplement Options",
-    imgUrl: "/assets/images/discover/1.png",
+    imgUrl:
+      "https://pi.ai/_next/image?url=https%3A%2F%2Fpi.ai%2Fpublic%2Fmedia%2Fguided-experiences%2Fventing.jpg&w=256&q=100",
     assistantID: "asst_yoLVpKKIOlDUgPmJDOHN88eh",
     baseContext: `Sure! Let’s create a personalized supplement plan for your PCOS type.
 
@@ -65,7 +67,8 @@ const DISVOCERS = [
   },
   {
     title: "Understand my PCOS type",
-    imgUrl: "/assets/images/discover/1.png",
+    imgUrl:
+      "https://pi.ai/_next/image?url=https%3A%2F%2Fpi.ai%2Fpublic%2Fmedia%2Fguided-experiences%2Fventing.jpg&w=256&q=100",
     assistantID: "asst_yoLVpKKIOlDUgPmJDOHN88eh",
     baseContext: `Sure! I'd love to help you understand your PCOS type. 
 
@@ -78,7 +81,8 @@ const DISVOCERS = [
   },
   {
     title: "Personalize a Workout Routine",
-    imgUrl: "/assets/images/discover/1.png",
+    imgUrl:
+      "https://pi.ai/_next/image?url=https%3A%2F%2Fpi.ai%2Fpublic%2Fmedia%2Fguided-experiences%2Fventing.jpg&w=256&q=100",
     assistantID: "asst_yoLVpKKIOlDUgPmJDOHN88eh",
     baseContext: `Definitely! I can help you create a custom workout routine based on your age, fitness and energy levels.
 
