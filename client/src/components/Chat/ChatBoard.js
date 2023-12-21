f92bdbd221770b14464201073f58ee732d254d52
messi9999 <micklepanda812@gmail.com>
2023-12-21 07:17:50 -0800

    udpated

--- client/src/components/Chat/ChatBoard.js
+++ client/src/components/Chat/ChatBoard.js
@@ -109,7 +109,7 @@ export default function ChatBoard() {
     };
 
     const header = {
-      Authorization: `Bearer ${currentUser.accessToken}`,
+      "x-access-token": `Bearer ${currentUser.accessToken}`,
     };
 
     try {
