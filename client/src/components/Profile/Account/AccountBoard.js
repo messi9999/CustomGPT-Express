fe3253bd427d31ff789f9cacbd9a159e353dfcbb
messi9999 <micklepanda812@gmail.com>
2023-12-24 21:37:32 -0800

    udpated

--- client/src/components/Profile/Account/AccountBoard.js
+++ client/src/components/Profile/Account/AccountBoard.js
@@ -8,8 +8,28 @@ export default function AccountBoard() {
   const currentUser = AuthService.getCurrentUser();
 
   return (
-      <div className="flex flex-col w-full">
-        <BackSideBar />
+      // <div className="flex flex-col w-full">
+      //   <BackSideBar />
+      //   <div className="flex flex-col w-full mx-16 my-16 h-screen">
+      //     <div className="flex flex-col text-[20px] lg:text-[28px] font-oswald mt-[100px]">
+      //       <div className="justify-start gap-x-6">
+      //         <label className="w-[150px] text-right font-bold">Username: </label>
+      //         <span>{currentUser.username}</span>
+      //       </div>
+      //       <div className="justify-start gap-x-6">
+      //         <label className="w-[150px] text-right font-bold">Email: </label>
+      //         <span>{currentUser.email}</span>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
+      <div className="flex flex-col w-full h-screen">
+      <BackSideBar />
+      <div className="flex justify-center items-center w-full h-full">
+        {/* <div className="mt-[10vh] w-[90%] lg:w-[60%] flex justify-center items-center lg:justify-between flex-col lg:flex-row gap-10">
+          <Subscription />
+        </div> */}
+        <div className="mt-[10vh] w-[90%] lg:w-[60%] flex justify-center items-center">
         <div className="flex flex-col w-full mx-16 my-16 h-screen">
           <div className="flex flex-col text-[20px] lg:text-[28px] font-oswald mt-[100px]">
             <div className="justify-start gap-x-6">
@@ -22,6 +42,8 @@ export default function AccountBoard() {
             </div>
           </div>
         </div>
+        </div>
       </div>
+    </div>
   );
 }
--- client/src/components/Profile/Payment/PaymentBoard.js
+++ client/src/components/Profile/Payment/PaymentBoard.js
@@ -5,10 +5,13 @@ import BackSideBar from "../BackSideBar";
 export default function PaymentBoard() {
   
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full h-screen">
       <BackSideBar />
-      <div className="flex justify-center w-full h-screen">
-        <div className="mt-[10vh] w-[90%] lg:w-[60%] flex items-center lg:justify-between flex-col lg:flex-row gap-10">
+      <div className="flex justify-center items-center w-full h-full">
+        {/* <div className="mt-[10vh] w-[90%] lg:w-[60%] flex justify-center items-center lg:justify-between flex-col lg:flex-row gap-10">
+          <Subscription />
+        </div> */}
+        <div className="mt-[10vh] w-[90%] lg:w-[60%] flex justify-center items-center">
           <Subscription />
         </div>
       </div>
