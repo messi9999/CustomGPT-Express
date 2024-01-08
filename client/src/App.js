b88e576f1e1b8ecbec88e3aa1eda57a20a5f8ee1
messi9999 <micklepanda812@gmail.com>
2024-01-07 20:10:15 -0800

    udpated

--- client/src/App.js
+++ client/src/App.js
@@ -200,7 +200,7 @@ const ProtectedRoute = ({ isAuthenticated, isPayment }) => {
 const ProtectLogOutRoute = ({ isAuthenticated }) => {
   if (isAuthenticated) {
     // Redirect them to the /login page, but save the current location they were trying to go to
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/dashboard" replace />;
   }
 
   
@@ -264,7 +264,7 @@ function App() {
                 >
                   <Route path="/login" element={<Login />} />
                   <Route path="/register" element={<Register />} />
-                </Route>
+                {/* </Route> */}
               </Routes>
             </Router>
         </DisplayTextContext.Provider>
