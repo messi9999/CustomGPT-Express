60d768caae867c991298684febd0d3ac331101b6
messi9999 <micklepanda812@gmail.com>
2024-01-11 17:34:38 -0800

    udpated

--- app.js
+++ app.js
@@ -24,24 +24,24 @@ app.use(express.urlencoded({ extended: true }));
 const db = require("./server/models");
 const Role = db.role;
 
-db.sequelize.sync();
+// db.sequelize.sync();
 
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and Resync DB.");
-//   initial();
-// });
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync DB.");
+  initial();
+});
 
-// function initial() {
-//   Role.create({
-//     id: 1,
-//     name: "user",
-//   });
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
 
-//   Role.create({
-//     id: 2,
-//     name: "admin",
-//   });
-// }
+  Role.create({
+    id: 2,
+    name: "admin",
+  });
+}
 
 //-----------Config Assistants----------------
 // const chatGptUtils = require("./server/utils/chatgpt.utils")
