14113bbc38116d787f4dc9eacd5079f7c9316dba
messi9999 <micklepanda812@gmail.com>
2024-07-01 07:31:23 -0700

    udpated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'server',
-        script: 'server.js',
+        script: 'app.js',
         instances: 1,
         autorestart: true,
         watch: false,
