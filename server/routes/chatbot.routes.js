f39577044c119f8ff207816560aafe91755e0aa0
messi9999 <micklepanda812@gmail.com>
2024-07-25 00:15:46 -0700

    udpated auth

--- server/routes/chatbot.routes.js
+++ server/routes/chatbot.routes.js
@@ -3,18 +3,5 @@ const { authJwt,  paymentCheck} = require("../middleware");
 
 var router = require("express").Router();
 
-module.exports = (app) => {
-  router.post(
-    "/getResponseFromGpt",
-    [authJwt.verifyToken, paymentCheck.checkPaymentExpiration],
-    controller.getResponseFromGpt
-  );
-  router.get("/createThread", controller.createThread);
-  router.post(
-    "/getChatHistory",
-    [authJwt.verifyToken],
-    controller.getChatHistory
-  )
-
   app.use("/api/chatbots", router);
 };
