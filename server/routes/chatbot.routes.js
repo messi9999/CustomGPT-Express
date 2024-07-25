78025704a8ec4aae372cc346529ce61e2df3f49b
messi9999 <micklepanda812@gmail.com>
2024-07-25 00:18:56 -0700

    udpated

--- server/routes/chatbot.routes.js
+++
@@ -1,7 +0,0 @@
-const controller = require("../controller/chatbot.controller");
-const { authJwt,  paymentCheck} = require("../middleware");
-
-var router = require("express").Router();
-
-  app.use("/api/chatbots", router);
-};
